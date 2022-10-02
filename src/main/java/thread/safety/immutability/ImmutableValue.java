1e887e567e4cbb124ef658150de78da528276a92
vkarikov <vkarikov@karikov.ru>
2022-10-02 10:28:22 +0300

    Immutability

---
+++ src/main/java/thread/safety/immutability/ImmutableValue.java
@@ -0,0 +1,20 @@
+package thread.safety.immutability;
+
+public class ImmutableValue{
+
+    private int value = 0;
+
+    public ImmutableValue(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+
+    public ImmutableValue add(int valueToAdd){
+        return new ImmutableValue(this.value + valueToAdd);
+    }
+
+}
---
+++ src/main/java/thread/safety/immutability/NotThreadSafeCalculator.java
@@ -0,0 +1,17 @@
+package thread.safety.immutability;
+
+public class NotThreadSafeCalculator {
+    private ImmutableValue currentValue = null;
+
+    public ImmutableValue getValue(){
+        return currentValue;
+    }
+
+    public void setValue(ImmutableValue newValue){
+        this.currentValue = newValue;
+    }
+
+    public void add(int newValue){
+        this.currentValue = this.currentValue.add(newValue);
+    }
+}
